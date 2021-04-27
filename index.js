beed3db116e78053f1f011c63c7ad66a5c653007
Muhammad Zahid <muhammadali775@gmail.com>
2021-04-27 01:14:57 -0400

    completed assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 8;
+
+let num2 = 7.75;
+
+let num3 = 16;
+
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+// test 2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+//test 3
+
+let mod = 16%6;
+
+
+//test4
+
+let max = Math.max(1, 2, 20);
